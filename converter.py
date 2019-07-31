3571a9f2f95cf0a338e0fc85b545cc96bcf2820b
Al Motion <slmkitani@gmail.com>
2019-07-31 20:36:00 +0200

    initial commit

--- converter.py
+++ converter.py
@@ -8,13 +8,13 @@ print(clip)
 def gifMaker(inputPath, targetFormat):
     outputPath = os.path.splitext(inputPath)[0] + targetFormat
 
-    # read the content of the clip
+    # read the content of the clip.
     frames = imageio.get_reader(inputPath)
 
-    # get the FPS from the Clip to generate Gif with the same FPS value
+    # get the FPS from the Clip to generate Gif with the same FPS value.
     fps = frames.get_meta_data()['fps']
 
-    # the new Clip configuration
+    # the new Clip configuration.
     writer = imageio.get_writer(outputPath, fps=fps)
 
     for frame in frames:
